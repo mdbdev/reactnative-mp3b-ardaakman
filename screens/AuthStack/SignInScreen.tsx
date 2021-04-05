0857c823a0d1ffdc4c54d61a8de0e683926cbf99
ArdaAkman <ardaakman16@gmail.com>
2021-04-04 17:38:05 -0700

    done stuff

--- screens/AuthStack/SignInScreen.tsx
+++ screens/AuthStack/SignInScreen.tsx
@@ -4,12 +4,17 @@ import { SafeAreaView, StyleSheet, ScrollView, Text } from "react-native";
 import { Appbar, TextInput, Snackbar, Button } from "react-native-paper";
 import { AuthStackParamList } from "./AuthStackScreen";
 import firebase from "firebase";
+import SignUpScreen from "./SignUpScreen";
 
 interface Props {
   navigation: StackNavigationProp<AuthStackParamList, "SignInScreen">;
 }
 
 export default function SignInScreen({ navigation }: Props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [visible, setVisible] = useState(false);
+  const [errMessage, setErr] = useState("");
   /* Screen Requirements:
       - AppBar
       - Email & Password Text Input
@@ -24,10 +29,66 @@ export default function SignInScreen({ navigation }: Props) {
     All authentication logic can be found at:
       https://firebase.google.com/docs/auth/web/starts
   */
+
+  const snackBarError = (message : String) => {
+  
+    const onToggleSnackBar = () => setVisible(!visible);
+  
+    const onDismissSnackBar = () => setVisible(false);
+  
+    return (
+        <Snackbar
+          visible={visible}
+          onDismiss={onDismissSnackBar}
+          action={{
+            label: 'Close',
+            onPress: () => { {setVisible(false)};
+            },
+          }}>
+          {message}
+        </Snackbar>
+    );
+  };
+
+  const errorDeal = (message: any) => {
+    setVisible(true);
+    setErr(message);
+  }
+
+
+  const signIn = () => {
+  firebase.auth().signInWithEmailAndPassword(email, password).then((userCredential) => {
+  }).catch(error => errorDeal(error.message));
+  }
+
+  const resetPassword = () => {
+    firebase.auth().sendPasswordResetEmail(email).then(function() {
+    setErr("A password reset email has been sent to you")
+    setVisible(true);
+    }).catch(error => errorDeal(error.message));
+  }
+
   return (
     <>
       <SafeAreaView style={styles.container}>
-        <Text>{"TODO"}</Text>
+        <Appbar.Header>
+          <Appbar.Content title = "Sign in" />
+        </Appbar.Header>
+        <TextInput
+        label = "Email"
+        value = {email}
+        onChangeText = {text => setEmail(text)}
+        />
+        <TextInput
+        secureTextEntry = {true}
+        label = "Password"
+        value = {password}
+        onChangeText = {text => setPassword(text)}
+        />
+        <Button onPress = {() => signIn()}> Sign In</Button>
+        <Button onPress = {() => navigation.navigate("SignUpScreen")}> Create an Account</Button>
+        <Button onPress = {() => resetPassword()}> Reset Password </Button>
+        {snackBarError(errMessage)}
       </SafeAreaView>
     </>
   );
--- screens/AuthStack/SignUpScreen.tsx
+++ screens/AuthStack/SignUpScreen.tsx
@@ -10,6 +10,10 @@ interface Props {
 }
 
 export default function SignUpScreen({ navigation }: Props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [visible, setVisible] = useState(false);
+  const [errMessage, setErr] = useState("");
   /* Screen Requirements:
       - AppBar
       - Email & Password Text Input
@@ -23,14 +27,63 @@ export default function SignUpScreen({ navigation }: Props) {
     All authentication logic can be found at:
       https://firebase.google.com/docs/auth/web/start
   */
-  return (
-    <>
-      <SafeAreaView style={styles.container}>
-        <Text>{"TODO"}</Text>
-      </SafeAreaView>
-    </>
-  );
-}
+
+  const snackBarError = (message : String) => {
+    const onToggleSnackBar = () => setVisible(!visible);
+    const onDismissSnackBar = () => setVisible(false);
+
+    return (
+      <Snackbar
+         visible={visible}
+          onDismiss={onDismissSnackBar}
+          action={{
+             label: 'Close',
+              onPress: () => { {setVisible(false)};
+                },
+              }}>
+            {message}
+      </Snackbar>
+        );
+      };
+    
+  const errorDeal = (message: any) => {
+    setVisible(true);
+    setErr(message);
+  }
+
+
+  const signUp = () => {
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((userCredential) => {}).catch(error => errorDeal(error.message))
+  }
+
+    
+    
+      return (
+        <>
+          <SafeAreaView style={styles.container}>
+            <Appbar.Header>
+              <Appbar.Content title = "Create an Account" />
+            </Appbar.Header>
+            <TextInput
+            label = "Email"
+            value = {email}
+            onChangeText = {text => setEmail(text)}
+            />
+            <TextInput
+            secureTextEntry = {true}
+            label = "Password"
+            value = {password}
+            onChangeText = {text => setPassword(text)}
+            />
+            <Button onPress = {() => signUp()}>  Create an Account</Button>
+            <Button onPress = {() => navigation.navigate("SignInScreen")}> or Sign in Instead</Button>
+            {snackBarError(errMessage)}
+          </SafeAreaView>
+        </>
+      );
+    }
+    
 
 const styles = StyleSheet.create({
   container: {
